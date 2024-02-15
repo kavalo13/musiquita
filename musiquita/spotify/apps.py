ed248a04aae7105eda2c08359961b9a7298097bd
Ozzy Flores <ozzy.a.flores@gmail.com>
2024-02-14 22:51:11 -0500

    Spotify results delivered

--- musiquita/spotify/apps.py
+++ musiquita/spotify/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SpotipyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "spotipy"
+    name = "spotify"
--- musiquita/spotify/views.py
+++ musiquita/spotify/views.py
@@ -24,7 +24,9 @@ def results(request):
             album_release_date=album["release_date"]
             album_id=album["id"]
             date_limit= datetime.now() - timedelta(7)
-            album_release_date_time= datetime.strptime(album_release_date,'%Y-%m-%d')
+            print("Album name" + album_name)
+            print("Album release date" + album_release_date)
+            album_release_date_time= try_parsing_date(album_release_date)
             if album_release_date_time >= date_limit:
                 album_tracks=sp.album_tracks(album["id"])
                 album_songs= album_tracks["items"]
@@ -37,7 +39,7 @@ def results(request):
 
 
 
-    context={"song names":new_songs}
+    context={"song_names":new_songs}
     return render(request,"results.html", context)
 # This function calls HttpResponse and simply returns 'Musiquita Homepage'
 # def homepage(request):
@@ -46,3 +48,12 @@ def results(request):
 # This function calls render and returns the 'homepage.html' file
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def try_parsing_date(text):
+    for fmt in ('%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y', '%Y'):
+        try:
+            return datetime.strptime(text, fmt)
+        except ValueError:
+            pass
+    raise ValueError('no valid date format found')
